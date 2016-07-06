8a2f240d20cc7e92a72fd61a81c2a0a75fa1b521
jiao-lc <939268350@qq.com>
2016-07-06 23:38:02 +0800

    [35. Search Insert Position][Accepted]committed by LeetCode Extension

---
+++ 35-Search-Insert-Position/solution.java
@@ -0,0 +1,19 @@
+public class Solution {
+    public int searchInsert(int[] nums, int target) {
+        int low = 0;
+        int high = nums.length - 1;
+
+        while (low <= high) {
+            int mid = (low + high) >>> 1;
+            int midVal = nums[mid];
+
+            if (midVal < target)
+                low = mid + 1;
+            else if (midVal > target)
+                high = mid - 1;
+            else
+                return mid;
+        }
+        return low;
+    }
+}
