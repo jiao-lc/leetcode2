8562d69ed1b6fdd8fb92e92b897888311948524a
jiao-lc <939268350@qq.com>
2016-07-07 22:16:51 +0800

    [58. Length of Last Word][Runtime Error]committed by LeetCode Extension

---
+++ 58-Length-of-Last-Word/solution.java
@@ -0,0 +1,13 @@
+public class Solution {
+    public int lengthOfLastWord(String s) {
+        char[] str = s.toCharArray();
+        if(str.length == 0) return 0;
+        int cnt = 0;
+        for(int i = str.length - 1; i >= 0; i++) {
+            if(str[i] == ' ' && cnt == 0)   continue;
+            if(str[i] == ' ' && cnt > 0)    break;
+            cnt++;
+        }
+        return cnt;
+    }
+}
