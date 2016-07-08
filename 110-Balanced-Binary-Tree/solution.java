974dcaa29f0652abd16b197742c88a5a0955e5ca
jiao-lc <939268350@qq.com>
2016-07-08 11:03:42 +0800

    [110. Balanced Binary Tree][Accepted]committed by LeetCode Extension

--- 110-Balanced-Binary-Tree/solution.java
+++ 110-Balanced-Binary-Tree/solution.java
@@ -9,11 +9,15 @@
  */
 public class Solution {
     public boolean isBalanced(TreeNode root) {
-        if(root == null)    return true;
-        return isBalanced(root.left) && isBalanced(root.right) && Math.abs(dpth(root.left) - dpth(root.right)) <= 1;
+        return check(root) != -1;
     }
-    public int dpth(TreeNode root) {
+    public int check(TreeNode root) {
         if(root == null)    return 0;
-        return 1 + Math.max(dpth(root.left), dpth(root.right));
+        int left = check(root.left);
+        if(left == -1)  return -1;
+        int right = check(root.right);
+        if(right == -1) return -1;
+        if(Math.abs(left - right) > 1) return -1;
+        return Math.max(left, right) + 1;
     }
 }
