d6da4cc6ed4dd3cc31f69dead7b236bd1e83339b
jiao-lc <939268350@qq.com>
2016-07-11 22:57:45 +0800

    [127. Word Ladder][Runtime Error]committed by LeetCode Extension

---
+++ 127-Word-Ladder/solution.java
@@ -0,0 +1,36 @@
+public class Solution {
+    public int ladderLength(String beginWord, String endWord, Set<String> wordList) {
+        int[] step = new int[1];
+        Set<String> used = new HashSet<String>();
+        helper(beginWord, endWord, wordList, used, step);
+        return step[0];
+    }
+    public void helper(String cur, String end, Set<String> wordList, Set<String> used, int[] step) {
+        if(cur.equals(end)) {
+            return;
+        }
+        if(used.size() == wordList.size()) {
+            return;
+        }
+        for(String word : wordList) {
+            if(oneDiff(word, cur)) {
+                used.add(cur);
+                step[0]++;
+                helper(word, end, wordList, used, step);
+                step[0]--;
+                used.remove(cur);
+            }
+        }
+    }
+    
+    public boolean oneDiff(String w1, String w2) {
+        if(w1.length() != w2.length())  return false;
+        char[] c1 = w1.toCharArray();
+        char[] c2 = w2.toCharArray();
+        int cnt = 0;
+        for(int i = 0; i < c1.length; i++) {
+            if(c1[i] != c2[i])  cnt++;
+        }
+        return cnt == 1;
+    }
+}
