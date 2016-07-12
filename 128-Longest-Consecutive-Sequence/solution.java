6c69d6619abe33f7ae772dc8e38cfc8598308158
jiao-lc <939268350@qq.com>
2016-07-12 16:46:05 +0800

    [128. Longest Consecutive Sequence][Accepted]committed by LeetCode Extension

--- 128-Longest-Consecutive-Sequence/solution.java
+++ 128-Longest-Consecutive-Sequence/solution.java
@@ -1,35 +1,17 @@
 public class Solution {
     public int longestConsecutive(int[] nums) {
+        int res = 0;
         HashMap<Integer, Integer> map = new HashMap<>();
         for(int num : nums) {
-            if(!map.containsKey(num + 1) && !map.containsKey(num - 1)) {
-                map.put(num, num);
-            } else if(!map.containsKey(num + 1)) {
-                map.put(num, map.get(num - 1));
-            } else if(!map.containsKey(num - 1)) {
-                map.put(num, map.get(num + 1));
-            } else {
-                map.put(num, map.get(num - 1));
-                map = update(map, num + 1);
-            }
+            if(map.containsKey(num)) continue;
+            int left = map.containsKey(num - 1) ? map.get(num - 1) : 0;
+            int right = map.containsKey(num + 1) ? map.get(num + 1) : 0;
+            int sum = left + right + 1;
+            res = Math.max(res, sum);
+            map.put(num, sum);
+            map.put(num - left, sum);
+            map.put(num + right, sum);
         }
-        int max = 0;
-        HashMap<Integer, Integer> cnt = new HashMap<>(); 
-        for(Integer i : map.values()) {
-            if(!cnt.containsKey(i)) {
-                cnt.put(i, 0);
-            }
-            cnt.put(i, cnt.get(i) + 1);
-            max = Math.max(max, cnt.get(i));
-        }
-        return max;
-    }
-    public HashMap<Integer, Integer> update(HashMap<Integer, Integer> map, int num) {
-        int target = map.get(num - 1);
-        while(map.containsKey(num)) {
-            map.put(num, target);
-            num++;
-        }
-        return map;
+        return res;
     }
 }
