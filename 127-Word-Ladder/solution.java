52237124b139bc60f90abbf3f95cd60d3e716fbe
jiao-lc <939268350@qq.com>
2016-07-12 15:15:04 +0800

    [127. Word Ladder][Accepted]committed by LeetCode Extension

--- 127-Word-Ladder/solution.java
+++ 127-Word-Ladder/solution.java
@@ -1,5 +1,6 @@
 public class Solution {
     public int ladderLength(String beginWord, String endWord, Set<String> wordList) {
+        wordList.add(endWord);
         Queue<String> myQueue = new LinkedList<String>();
         myQueue.offer(beginWord);
         int level = 1;
@@ -9,26 +10,20 @@ public class Solution {
             for(int i = 0; i < qSize; i++) {
                 String tmp = myQueue.poll();
                 if(tmp.equals(endWord))    return level;
-                for(String word : wordList) {
-                    if(oneDiff(word, tmp)) {
-                        myQueue.offer(word);
-                        used.remove(word);
+                for(int j = 0; j < tmp.length(); j++) {
+                    char[] arr = tmp.toCharArray();
+                    for(char c = 'a'; c <= 'z'; c++) {
+                        arr[j] = c;
+                        String check = new String(arr);
+                        if(!check.equals(tmp) && wordList.contains(check)){
+                            myQueue.add(check);
+                            wordList.remove(check);
+                        }
                     }
                 }
-                wordList = used;
             }
             level++;
         }
         return 0;
-    }
-    public boolean oneDiff(String w1, String w2) {
-        if(w1.length() != w2.length())  return false;
-        char[] c1 = w1.toCharArray();
-        char[] c2 = w2.toCharArray();
-        int cnt = 0;
-        for(int i = 0; i < c1.length; i++) {
-            if(c1[i] != c2[i])  cnt++;
-        }
-        return cnt == 1;
     }
 }
