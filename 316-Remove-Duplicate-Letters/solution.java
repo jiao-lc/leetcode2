6fc43cebbc6347070e0b7d1bff14dbd028d9882d
jiao-lc <939268350@qq.com>
2016-07-13 10:30:38 +0800

    [316. Remove Duplicate Letters][Accepted]committed by LeetCode Extension

--- 316-Remove-Duplicate-Letters/solution.java
+++ 316-Remove-Duplicate-Letters/solution.java
@@ -2,11 +2,24 @@ public class Solution {
     public String removeDuplicateLetters(String s) {
         int[] count = new int[26];
         int pos = 0;
-        for(int i=0; i<s.length(); i++) count[s.charAt(i) - 'a']++;
-        for(int i=0; i<s.length(); i++) {
-            if(s.charAt(i)< s.charAt(pos)) pos = i;
-            if(--count[s.charAt(i) - 'a'] == 0) break;
+        boolean[] visited = new boolean[26];
+        Stack<Character> st = new Stack<Character>();
+        char[] ch = s.toCharArray();
+        for(char c : ch) {
+            count[c - 'a']++;
         }
-        return s.length() == 0? "" : s.charAt(pos) + removeDuplicateLetters(s.substring(pos+1).replaceAll(""+ s.charAt(pos), ""));
+        for(char c : ch) {
+            pos = c - 'a';
+            count[pos]--;
+            if(visited[pos]) continue;
+            while(!st.empty() && st.peek() > c  && count[st.peek() - 'a'] > 0) visited[st.pop() - 'a'] = false;
+            st.push(c);
+            visited[pos] = true;
+        }
+        StringBuilder sb = new StringBuilder();
+        while(!st.empty()){
+            sb.insert(0,st.pop());
+        }
+        return sb.toString();
     }
 }
