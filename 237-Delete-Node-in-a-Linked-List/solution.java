b123f926dafecd98259bf8288d14731138fc63c4
jiao-lc <939268350@qq.com>
2016-07-14 23:20:56 +0800

    [237. Delete Node in a Linked List][Accepted]committed by LeetCode Extension

--- 237-Delete-Node-in-a-Linked-List/solution.java
+++ 237-Delete-Node-in-a-Linked-List/solution.java
@@ -8,9 +8,12 @@
  */
 public class Solution {
     public void deleteNode(ListNode node) {
+        ListNode prev = node;
         while(node.next != null) {
             node.val = node.next.val;
+            prev = node;
+            node = node.next;
         }
-        node = null;
+        prev.next = null;
     }
 }
