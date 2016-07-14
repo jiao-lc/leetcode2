076db58af11507c76992fb635f0f8eab03a44b21
jiao-lc <939268350@qq.com>
2016-07-14 23:15:57 +0800

    [222. Count Complete Tree Nodes][Accepted]committed by LeetCode Extension

--- 222-Count-Complete-Tree-Nodes/solution.java
+++ 222-Count-Complete-Tree-Nodes/solution.java
@@ -8,20 +8,32 @@
  * }
  */
 public class Solution {
-    public int countNodes(TreeNode root) {
-        if(root == null) return 0;
-        int left = 0, right = 0;
-        TreeNode node = root;
-        while(node != null) {
-            node = node.left;
-            left++;
-        }
-        node = root;
-        while(node != null) {
-            node = node.right;
-            right++;
-        }
-        if(left == right && left != 0) return (1 << left) - 1;
-        return 1 + countNodes(root.left) + countNodes(root.right);
+public int countNodes(TreeNode root) {
+	if (root==null) return 0;
+	if (root.left==null) return 1;
+	int height = 0;
+    int nodesSum = 0;
+	TreeNode curr = root;
+    while(curr.left!=null) {
+    	nodesSum += (1<<height);
+    	height++;
+    	curr = curr.left;
     }
+    return nodesSum + countLastLevel(root, height);
+}
+
+private int countLastLevel(TreeNode root, int height) {
+	if(height==1) 
+		if (root.right!=null) return 2;
+		else if (root.left!=null) return 1;
+		else return 0;
+	TreeNode midNode = root.left;
+	int currHeight = 1;
+	while(currHeight<height) {
+		currHeight++;
+		midNode = midNode.right;
+	}
+	if (midNode==null) return countLastLevel(root.left, height-1);
+	else return (1<<(height-1)) + countLastLevel(root.right, height-1);
+}
 }
