edd8fee234db666f1762162b7d59ecc5f70aa72a
jiao-lc <939268350@qq.com>
2016-07-14 16:32:28 +0800

    [207. Course Schedule][Accepted]committed by LeetCode Extension

--- 207-Course-Schedule/solution.java
+++ 207-Course-Schedule/solution.java
@@ -1,28 +1,35 @@
 public class Solution {
     public boolean canFinish(int numCourses, int[][] prerequisites) {
         ArrayList[] graph = new ArrayList[numCourses];
+        int[] degree = new int[numCourses];
+        Queue queue = new LinkedList();
+        int count = 0;
         for(int i=0;i<numCourses;i++)
             graph[i] = new ArrayList();
-            
-        boolean[] visited = new boolean[numCourses];
-        for(int i=0; i<prerequisites.length;i++){
-            graph[prerequisites[i][1]].add(prerequisites[i][0]);
+        for(int[] prerequisit : prerequisites) {
+            degree[prerequisit[1]]++;
+            graph[prerequisit[0]].add(prerequisit[1]);
         }
-        for(int i=0; i<numCourses; i++){
-            if(!dfs(graph,visited,i))
-                return false;
+        for(int i=0; i<degree.length;i++){
+            if(degree[i] == 0){
+                queue.add(i);
+                count++;
+            }
         }
-        return true;
-    }
-    
-    public boolean dfs(ArrayList[] graph, boolean[] visited, int num) {
-        if(visited[num]) return false;
-        visited[num] = true;
-        for(int i = 0; i < graph[num].size(); i++) {
-            if(!dfs(graph,visited,(int)graph[num].get(i)))
-                return false;
+        while(queue.size() != 0){
+            int course = (int)queue.poll();
+            for(int i=0; i<graph[course].size();i++){
+                int pointer = (int)graph[course].get(i);
+                degree[pointer]--;
+                if(degree[pointer] == 0){
+                    queue.add(pointer);
+                    count++;
+                }
+            }
         }
-        visited[num] = false;
-        return true;
+        if(count == numCourses)
+            return true;
+        else    
+            return false;
     }
 }
