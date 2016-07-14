4bf3c1c8b9ba91c295ad3623a8a38348326e3887
jiao-lc <939268350@qq.com>
2016-07-14 10:44:47 +0800

    [189. Rotate Array][Accepted]committed by LeetCode Extension

--- 189-Rotate-Array/solution.java
+++ 189-Rotate-Array/solution.java
@@ -1,18 +1,32 @@
 public class Solution {
-public void rotate(int[] nums, int k) {
-    k %= nums.length;
-    reverse(nums, 0, nums.length - 1);
-    reverse(nums, 0, k - 1);
-    reverse(nums, k, nums.length - 1);
-}
-
-public void reverse(int[] nums, int start, int end) {
-    while (start < end) {
-        int temp = nums[start];
-        nums[start] = nums[end];
-        nums[end] = temp;
-        start++;
-        end--;
+    public void rotate(int[] nums, int k) {
+        if(nums.length <= 1){
+            return;
+        }
+        //step each time to move
+        int step = k % nums.length;
+        //find GCD between nums length and step
+        int gcd = findGcd(nums.length, step);
+        int position, count;
+        
+        //gcd path to finish movie
+        for(int i = 0; i < gcd; i++){
+            //beginning position of each path
+            position = i;
+            //count is the number we need swap each path
+            count = nums.length / gcd - 1;
+            for(int j = 0; j < count; j++){
+                position = (position + step) % nums.length;
+                //swap index value in index i and position
+                nums[i] ^= nums[position];
+                nums[position] ^= nums[i];
+                nums[i] ^= nums[position];
+            }
+        }
     }
-}
+    
+    public int findGcd(int a, int b){
+        return (a == 0 || b == 0) ? a + b : findGcd(b, a % b);
+    }
+    
 }
