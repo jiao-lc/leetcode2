9040d16797f8f62e7a8f2be4e42132cf980bf942
jiao-lc <939268350@qq.com>
2016-07-15 10:10:41 +0800

    [218. The Skyline Problem][Accepted]committed by LeetCode Extension

--- 218-The-Skyline-Problem/solution.java
+++ 218-The-Skyline-Problem/solution.java
@@ -1,31 +1,77 @@
 public class Solution {
     public List<int[]> getSkyline(int[][] buildings) {
-        List<int[]> heights = new ArrayList<int[]>();
-        List<int[]> res = new ArrayList<int[]>();
-        for(int[] build : buildings) {
-            heights.add(new int[]{build[0], -build[2]});
-            heights.add(new int[]{build[1], build[2]});
+        Segment seg = new Segment();
+        for (int[] input: buildings)
+            seg.add(input[0], input[1], input[2]);
+            
+        ArrayList<int[]> list = new ArrayList<int[]>();
+        Node node = seg.root;
+        int lastHeight = -1;
+        while (node != null) {
+            int[] arr = new int[2];
+            arr[0] = node.start;
+            arr[1] = node.height;
+            if ((arr[0] != 0 || arr[1] != 0) && arr[1]!=lastHeight)
+                list.add(arr);
+            lastHeight = arr[1];
+            node = node.next;
         }
-        Collections.sort(heights, (a, b) -> {
-            if(a[0] != b[0]) 
-                return a[0] - b[0];
-            return a[1] - b[1];
-        });
-        Queue<Integer> pq = new PriorityQueue<>((a, b) -> (b - a));
-        pq.offer(0);
-        int prev = 0;
-        for(int[] height : heights) {
-            if(height[1] < 0) {
-                pq.offer(-height[1]);
+        return list;
+    }
+}
+
+class Node {
+    int start;
+    int height;
+    Node next;
+    public Node(int s, int h) {
+        this.start = s;
+        this.height = h;
+    }
+}
+
+class Segment {
+    public Node root = null;
+    Node last = null;
+    public Segment() {
+        root = new Node(0, 0);
+        last = root;
+    }
+    
+    public void add(int begin, int end, int h) {
+        Node node = last;
+        while (node.start < begin && node.next != null && node.next.start <= begin) {
+            node = node.next;
+        }
+        
+        last = node;
+        int lastHeight = node.height;
+        if (node.start == begin) {
+            node.height = Math.max(node.height, h);
+        } else {
+            if (node.height < h) {
+                Node tem = node.next;
+                node.next = new Node(begin, h);
+                node.next.next = tem;
+                last = node.next;
+            }
+        }
+        
+        node = last;
+        while (node.next != null && node.next.start < end) {
+            lastHeight = node.next.height;
+            if (node.height == node.next.height) {
+                node.next = node.next.next;
             } else {
-                pq.remove(height[1]);
-            }
-            int cur = pq.peek();
-            if(cur != prev) {
-                res.add(new int[]{height[0], cur});
-                prev = cur;
+                if (node.next.height < h)
+                    node.next.height = h;
+                node = node.next;
             }
         }
-        return res;
+        if ((node.next == null || node.next.start > end) && lastHeight < h) {
+            Node tem = node.next;
+            node.next = new Node(end, lastHeight);
+            node.next.next = tem;
+        }
     }
 }
