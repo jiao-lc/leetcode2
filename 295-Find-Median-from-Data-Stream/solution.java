efd2ec08795b1e5379c02b3479e9c8096595e48d
jiao-lc <939268350@qq.com>
2016-07-15 11:16:29 +0800

    [295. Find Median from Data Stream][Accepted]committed by LeetCode Extension

--- 295-Find-Median-from-Data-Stream/solution.java
+++ 295-Find-Median-from-Data-Stream/solution.java
@@ -1,38 +1,44 @@
-public class MedianFinder {
-    List<Integer> arr = new ArrayList<Integer>();
-    MedianFinder() {
-        arr = new ArrayList<Integer>();
+class MedianFinder {
+
+    PriorityQueue<Integer> leftQ;
+    PriorityQueue<Integer> rightQ;
+    Double median = null;
+
+    public MedianFinder(){
+        leftQ = new PriorityQueue<>(Comparator.reverseOrder());
+        rightQ = new PriorityQueue<>();
     }
+
     // Adds a number into the data structure.
     public void addNum(int num) {
-        int i = 0;
-        for(; i < arr.size(); i++) {
-            if(arr.get(i) > num) break;
+        if(median == null){
+            leftQ.offer(num);
+            median = num*1.0;
+        } else if(num > median){
+            rightQ.offer(num);
+            median = findMedian(rightQ, leftQ);
+            
+        } else {
+            leftQ.offer(num);
+            median = findMedian(leftQ, rightQ);
         }
-        if(i == arr.size()) arr.add(num);
-        else {
-            arr.add(i, num);
-        }
+            
+    }
+    
+    private double findMedian(PriorityQueue<Integer> leftQ, PriorityQueue<Integer> rightQ){
+        double result = 0;
+        if(leftQ.size() - rightQ.size() == 2)
+            rightQ.offer(leftQ.poll());
+            
+        if(leftQ.size() - rightQ.size() == 1)
+            result = leftQ.peek()*1.0;
+        else
+            result = (leftQ.peek() + rightQ.peek())*1.0 / 2;
+        return result;
     }
 
     // Returns the median of current data stream
     public double findMedian() {
-        int size = arr.size();
-        int mid = mid = size / 2;;
-        double res = 0;
-        if((size&1) == 1) {
-            res = (double)arr.get(mid);
-        }
-        else {
-            res += (double)arr.get(mid--);
-            res += (double)arr.get(mid);
-            res /= 2;
-        }
-        return res;
+        return median;
     }
 };
-
-// Your MedianFinder object will be instantiated and called as such:
-// MedianFinder mf = new MedianFinder();
-// mf.addNum(1);
-// mf.findMedian();
