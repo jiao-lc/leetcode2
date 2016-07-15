a58c3ef5ecdb8fecda4fc2d1ded5f2f722e80d20
jiao-lc <939268350@qq.com>
2016-07-15 10:08:50 +0800

    [218. The Skyline Problem][Accepted]committed by LeetCode Extension

--- 218-The-Skyline-Problem/solution.java
+++ 218-The-Skyline-Problem/solution.java
@@ -1,66 +1,31 @@
 public class Solution {
-    class SegmentTreeNode {
-	    int start;
-    	int end;
-    	int val;
-    	SegmentTreeNode left;
-    	SegmentTreeNode right;
-    	SegmentTreeNode(int s, int e, int v) { start = s; end = e; val = v;}
-    }
     public List<int[]> getSkyline(int[][] buildings) {
+        List<int[]> heights = new ArrayList<int[]>();
         List<int[]> res = new ArrayList<int[]>();
-        SegmentTreeNode root = new SegmentTreeNode(0, Integer.MAX_VALUE, 0);
         for(int[] build : buildings) {
-            root = insert(root, build[0], build[1], build[2]);
+            heights.add(new int[]{build[0], -build[2]});
+            heights.add(new int[]{build[1], build[2]});
         }
-        fill(res, root, 0);
-        if(res.get(res.size() - 1)[1] != 0)
-            res.add(new int[]{Integer.MAX_VALUE, 0});
+        Collections.sort(heights, (a, b) -> {
+            if(a[0] != b[0]) 
+                return a[0] - b[0];
+            return a[1] - b[1];
+        });
+        Queue<Integer> pq = new PriorityQueue<>((a, b) -> (b - a));
+        pq.offer(0);
+        int prev = 0;
+        for(int[] height : heights) {
+            if(height[1] < 0) {
+                pq.offer(-height[1]);
+            } else {
+                pq.remove(height[1]);
+            }
+            int cur = pq.peek();
+            if(cur != prev) {
+                res.add(new int[]{height[0], cur});
+                prev = cur;
+            }
+        }
         return res;
-    }
-    public SegmentTreeNode insert(SegmentTreeNode root, int s, int e, int v) {
-        if(root.start >= s && root.end <= e) {
-    		root.val = Math.max(root.val, v);
-    		return root;
-    	}
-    	if(root.end <= s || root.start >= e)	return root;
-    	if(root.start < s) {
-    		if(root.right == null){
-    			root.right = new SegmentTreeNode(s, root.end, root.val);
-    			root.left = new SegmentTreeNode(root.start, s, root.val);
-    		}
-    	} 
-    	if(root.end >= e) {
-    		if(root.left == null){
-    			root.left = new SegmentTreeNode(root.start, e, root.val);
-    			root.right = new SegmentTreeNode(e, root.end, root.val);
-    		}
-    	} 
-    	root.left = insert(root.left, s, e, v);
-		root.right = insert(root.right, s, e, v);
-    	return root;
-    }
-    public void fill(List<int[]> res, SegmentTreeNode root, int cur) {
-		if(root == null)  return;
-		Stack<SegmentTreeNode> st = new Stack<SegmentTreeNode>();
-		while(!st.empty() || root != null) {
-		    while(root != null) {
-		        st.push(root);
-		        root = root.left;
-		    }
-		    root = st.pop();
-		    if(isLeaf(root)) {
-		        if(root.val != cur) {
-		            res.add(new int[]{root.start, root.val});
-		            cur = root.val;
-		        }
-		    }
-		    root = root.right;
-		}
-	}
-    public boolean isLeaf(SegmentTreeNode node) {
-        if(node == null) return false;
-        if(node.left == null && node.right == null) return true;
-        return false;
     }
 }
