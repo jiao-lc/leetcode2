41e6a6a37f6869f174020d532b5c1efd67d624dc
jiao-lc <939268350@qq.com>
2016-07-17 10:46:14 +0800

    [328. Odd Even Linked List][Accepted]committed by LeetCode Extension

---
+++ 328-Odd-Even-Linked-List/solution.java
@@ -0,0 +1,24 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+public class Solution {
+    public ListNode oddEvenList(ListNode head) {
+        if(head == null) return null;
+        ListNode odd = head;
+        ListNode even = head.next;
+        ListNode o = odd, e = even;
+        while(o.next != null && e != null && e.next != null) {
+            o.next = e.next;
+            o = o.next;
+            e.next = o.next;
+            e = e.next;
+        }
+        o.next = even;
+        return odd;
+    }
+}
