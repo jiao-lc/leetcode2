155d303b467ee29dab6364f1bb4530ba8d06726c
jiao-lc <939268350@qq.com>
2016-07-18 21:40:48 +0800

    [336. Palindrome Pairs][Accepted]committed by LeetCode Extension

--- 336-Palindrome-Pairs/solution.java
+++ 336-Palindrome-Pairs/solution.java
@@ -1,41 +1,41 @@
 public class Solution {
-
 public List<List<Integer>> palindromePairs(String[] words) {
-    List<List<Integer>> ret = new ArrayList<>(); 
-    if (words == null || words.length < 2) return ret;
+    List<List<Integer>> res = new ArrayList<>(); 
+    if (words == null || words.length < 2) {
+        return res;
+    }
     Map<String, Integer> map = new HashMap<String, Integer>();
-    for (int i=0; i<words.length; i++) map.put(words[i], i);
-    for (int i=0; i<words.length; i++) {
-        // System.out.println(words[i]);
-        for (int j=0; j<=words[i].length(); j++) { // notice it should be "j <= words[i].length()"
+    for (int i = 0; i < words.length; i++) {
+        map.put(words[i], i);
+    }
+    for (int i = 0; i < words.length; i++) {
+        for (int j = 0; j <= words[i].length(); j++) {
             String str1 = words[i].substring(0, j);
             String str2 = words[i].substring(j);
-            if (isPalindrome(str1)) {
-                String str2rvs = new StringBuilder(str2).reverse().toString();
-                if (map.containsKey(str2rvs) && map.get(str2rvs) != i) {
-                    List<Integer> list = new ArrayList<Integer>();
-                    list.add(map.get(str2rvs));
-                    list.add(i);
-                    ret.add(list);
-                    // System.out.printf("isPal(str1): %s\n", list.toString());
-                }
-            }
-            if (isPalindrome(str2)) {
-                String str1rvs = new StringBuilder(str1).reverse().toString();
-                // check "str.length() != 0" to avoid duplicates
-                if (map.containsKey(str1rvs) && map.get(str1rvs) != i && str2.length()!=0) { 
-                    List<Integer> list = new ArrayList<Integer>();
-                    list.add(i);
-                    list.add(map.get(str1rvs));
-                    ret.add(list);
-                    // System.out.printf("isPal(str2): %s\n", list.toString());
-                }
+            addPair(map, res, str1, str2, i, false);
+            if(str2.length() != 0) {
+                addPair(map, res, str2, str1, i, true);
             }
         }
     }
-    return ret;
+    return res;
 }
-
+private void addPair(Map<String, Integer> map, List<List<Integer>> res, String str1, String str2, int index, boolean reverse) {
+    if (isPalindrome(str1)) {
+        String str2rev = new StringBuilder(str2).reverse().toString();
+        if (map.containsKey(str2rev) && map.get(str2rev) != index) {
+            List<Integer> list = new ArrayList<>();
+            if(!reverse) {
+                list.add(map.get(str2rev));
+                list.add(index);
+            } else {
+                list.add(index);
+                list.add(map.get(str2rev));
+            }
+            res.add(list);
+        }
+    }
+}
 private boolean isPalindrome(String str) {
     int left = 0;
     int right = str.length() - 1;
@@ -44,5 +44,4 @@ private boolean isPalindrome(String str) {
     }
     return true;
 }
-
 }
