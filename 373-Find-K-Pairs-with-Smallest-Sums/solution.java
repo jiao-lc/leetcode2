43795bf7fb6545723c83ade58519c13af4d008c3
jiao-lc <939268350@qq.com>
2016-07-19 12:07:05 +0800

    [373. Find K Pairs with Smallest Sums][Accepted]committed by LeetCode Extension

--- 373-Find-K-Pairs-with-Smallest-Sums/solution.java
+++ 373-Find-K-Pairs-with-Smallest-Sums/solution.java
@@ -1,15 +1,29 @@
 public class Solution {
     public List<int[]> kSmallestPairs(int[] nums1, int[] nums2, int k) {
-        PriorityQueue<int[]> queue = new PriorityQueue<int[]>(k, (a, b) -> ((a[0] + a[1]) - (b[0] + b[1])));
-        for(int i : nums1) {
-            for(int j : nums2) {
-                queue.add(new int[]{i, j});
+        PriorityQueue<int[]> heap = new PriorityQueue<int[]>(k,new Comparator<int[]>(){
+           public int compare(int[] a,int[] b){
+                return -a[0]-a[1]+b[0]+b[1];
+           } 
+        });
+        List<int[]> res = new ArrayList<>();
+        for(int i =0;i<k&&i<nums1.length;i++){
+            for(int j=0;j<nums2.length;j++){
+                int[] cur = new int[2];
+                cur[0] = nums1[i];
+                cur[1] = nums2[j];
+                if(heap.size()<k)
+                    heap.offer(cur);
+                else{
+                    int[] top = heap.peek();
+                    if(top[0]+top[1]>cur[0]+cur[1]){
+                        heap.poll();
+                        heap.offer(cur);
+                    }
+                }
             }
         }
-        List<int[]> res = new ArrayList<int[]>();
-        while(k > 0 && !queue.isEmpty()) {
-            res.add(queue.poll());
-            k--;
+        for(int[] a: heap){
+            res.add(a);
         }
         return res;
     }
