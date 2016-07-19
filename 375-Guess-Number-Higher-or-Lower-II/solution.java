d9d4cbe74a205f8d255aeff41894bbc2ab12593f
jiao-lc <939268350@qq.com>
2016-07-19 09:45:58 +0800

    [375. Guess Number Higher or Lower II][Accepted]committed by LeetCode Extension

--- 375-Guess-Number-Higher-or-Lower-II/solution.java
+++ 375-Guess-Number-Higher-or-Lower-II/solution.java
@@ -1,25 +1,16 @@
 public class Solution {
-  public int getMoneyAmount(int n) {
-    int[][] table = new int[n + 1][n + 1];  //0
-    return payForRange(table, 1, n);
-  }
-
-  //return the amount paid for the game within range [start,end]
-  private int payForRange(int[][] dp, int start, int end) {
-    if (start >= end)
-      return 0;
-    if (dp[start][end] != 0)
-      return dp[start][end];
-
-    int minimumForCurrentRange = Integer.MAX_VALUE;
-    for (int x = start; x <= end; ++x) {
-      //calculate the amount to pay if pick x.
-      int pay = x + Math.max(payForRange(dp, start, x - 1), payForRange(dp, x + 1, end));
-      //calculate min of maxes
-      minimumForCurrentRange = Math.min(minimumForCurrentRange, pay);
+   public int getMoneyAmount(int n) {
+        int[][] table = new int[n+1][n+1];
+        for(int j=2; j<=n; j++){
+            for(int i=j-1; i>0; i--){
+                int globalMin = Integer.MAX_VALUE;
+                for(int k=i+1; k<j; k++){
+                    int localMax = k + Math.max(table[i][k-1], table[k+1][j]);
+                    globalMin = Math.min(globalMin, localMax);
+                }
+                table[i][j] = i+1==j?i:globalMin;
+            }
+        }
+        return table[1][n];
     }
-
-    dp[start][end] = minimumForCurrentRange;
-    return minimumForCurrentRange;
-  }
 }
