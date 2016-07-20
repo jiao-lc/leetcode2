761f4c048fce7aeb332d446ae74753b6b4863b34
jiao-lc <939268350@qq.com>
2016-07-20 10:40:40 +0800

    [373. Find K Pairs with Smallest Sums][Accepted]committed by LeetCode Extension

--- 373-Find-K-Pairs-with-Smallest-Sums/solution.java
+++ 373-Find-K-Pairs-with-Smallest-Sums/solution.java
@@ -1,29 +1,45 @@
 public class Solution {
-    class Pair {
-        int idx1;
-        int idx2;
-        int sum;
-        Pair(int i, int j, int s) {
-            idx1 = i;
-            idx2 = j;
-            sum = s;
-        }
-    }
+    final int[][] neighbors = {{0, 1}, {1, 0}};
     public List<int[]> kSmallestPairs(int[] nums1, int[] nums2, int k) {
-        PriorityQueue<Pair> queue = new PriorityQueue<Pair>(k, (a, b) -> (a.sum - b.sum));
-        queue.offer(new Pair(0, 0, nums1[0] + nums2[0]));
-        Set<int[]> res = new HashSet<int[]>();
-        while(res.size() < k && !queue.isEmpty()) {
-            Pair tmp = queue.poll();
-            int x = tmp.idx1, y = tmp.idx2;
-            if(res.add(new int[]{nums1[x], nums2[y]})) {
-                if(x < nums1.length - 1)
-                    queue.offer(new Pair(x + 1, y, nums1[x + 1] + nums2[y]));
-                if(y < nums2.length - 1)
-                    queue.offer(new Pair(x, y + 1, nums1[x] + nums2[y + 1]));
+        List<int[]> list = new ArrayList<>();
+        if (nums1 == null || nums2 == null || nums1.length == 0 || nums2.length == 0 || k == 0) {
+            return list;
+        }
+        int m = nums1.length, n = nums2.length;
+        boolean[][] visited = new boolean[m][n];
+        Queue<Pair> minHeap = new PriorityQueue<>();
+        minHeap.offer(new Pair(0, 0, nums1[0] + nums2[0]));
+        visited[0][0] = true;
+        while (k > 0 && !minHeap.isEmpty()) {
+            Pair min = minHeap.poll();
+            list.add(new int[] {nums1[min.row], nums2[min.col]});
+            k--;
+            for (int[] neighbor : neighbors) {
+                int row1 = min.row + neighbor[0];
+                int col1 = min.col + neighbor[1];
+                if (row1 < 0 || row1 == m || col1 < 0 || col1 == n || visited[row1][col1]) {
+                    continue;
+                }
+                visited[row1][col1] = true;
+                minHeap.offer(new Pair(row1, col1, nums1[row1] + nums2[col1]));
             }
         }
-        List<int[]> ret = new ArrayList<int[]>(res);
-        return ret;
+        return list;
     }
+}
+
+class Pair implements Comparable<Pair> {
+    int row;
+    int col;
+    int value;
+    
+    Pair(int row, int col, int value) {
+        this.row = row;
+        this.col = col;
+        this.value = value;
+    }
+    
+    public int compareTo(Pair other) {
+        return value - other.value;
+    } 
 }
