c3bb5540c89aaa0cd2e7c3d49f4d6323599b1a99
jiao-lc <939268350@qq.com>
2016-08-30 01:10:58 +0800

    [384. Shuffle an Array][Accepted]committed by LeetCode Extension

--- 384-Shuffle-an-Array/solution.java
+++ 384-Shuffle-an-Array/solution.java
@@ -1,38 +1,25 @@
 public class Solution {
-    public int[] array;
-    public int[] original;
+    private int[] nums;
+    private int[] copy;
+
     public Solution(int[] nums) {
-        array = new int[nums.length];
-        original = new int[nums.length];
-        for(int i = 0; i < nums.length; i++) {
-            array[i] = nums[i];
-            original[i] = nums[i];
-        }
+        this.nums = nums;
+        this.copy = nums.clone();
     }
-    
-    /** Resets the array to its original configuration and return it. */
+
     public int[] reset() {
-        for(int i = 0; i < array.length; i++) {
-            array[i] = original[i];
-        }
-        return array;
+        return copy;
     }
-    
-    /** Returns a random shuffling of the array. */
+
     public int[] shuffle() {
-        for(int i = 1; i < array.length; i++) {
-            int k = rand(0, i);
-            int tmp = array[k];
-            array[k] = array[i];
-            array[i] = tmp;
+        Random random = new Random();
+        for (int i = nums.length - 1; i > 0; i--) {
+            int j = random.nextInt(i + 1);
+            int t = nums[i];
+            nums[i] = nums[j];
+            nums[j] = t;
         }
-        return array;
-    }
-    
-    public int rand(int low, int high) {
-        Random r = new Random();
-        int d = r.nextInt(high - low + 1);
-        return low + d;
+        return nums;
     }
 }
 
